1a56bfa19c09b59457b50f38b7dc09246b20241e
Staero123 <153693037+Staero123@users.noreply.github.com>
2023-12-13 17:05:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ import plotly.express as px # interactive charts
 
 # read csv from a github repo
 Flight_log = {}
-dirpath = "C:\Users\AIRSHOW\Desktop\DroPort LogsLogs\Charger 20230523_20231105 CSV\\"
+dirpath = r"C:\Users\AIRSHOW\Desktop\DroPort Logs\LogsCharger 20230523_20231105 CSV"
 for path in os.listdir(dirpath):
-    j=n[:-20]
-    Flight_log[j] = pd.read_csv("/Users/AIRSHOW/Desktop/New folder/"+path+'/'+x,sep=",",skiprows=3)
-
+    try:
+        n=path[0:58]
+        Flight_log[n] = pd.read_csv("/Users/AIRSHOW/Desktop/DroPort Logs/LogsCharger 20230523_20231105 CSV/"+n+'.txt.csv',sep=",",skiprows=3)
+    except:
+        pass
 
